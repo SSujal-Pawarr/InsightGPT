332f8c17500347241a148ae036c0b77b601898de
SujalPawar <pawarsujal4455@gmail.com>
2026-06-17 10:02:51 +0530

    Added Thread.js file

---
+++ Backend/models/Thread.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose"
+
+const MessageSchema= new mongoose.Schema({
+    role:{
+        type:String,
+        enum:["user","assistant"],
+        required:true
+    },
+    content:{
+        type:String
+        required:true
+    },
+    timeStamp:{
+        type:Date, 
+        default:Date.now
+    }
+
+});
