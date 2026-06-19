1a17210ea55857bcb0c15aaf2fe77b1482d7539e
SujalPawar <pawarsujal4455@gmail.com>
2026-06-19 17:22:21 +0530

    modify chat.js file

--- Backend/routes/chat.js
+++ Backend/routes/chat.js
@@ -2,3 +2,20 @@ import express from "express";
 import Thread from "../models/Thread.js";
 
 const router=express.Router();
+
+//test
+router.post("/test",async(requestAnimationFrame,res)=>{
+    try{
+        const thread=new Thread({
+            threadId:"xyz",
+            title:"Testing new Thread"
+        });
+        const response=await thread.save();
+        res.send(response);
+
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error:"Failed to save in DB"});
+        
+    }
+});
