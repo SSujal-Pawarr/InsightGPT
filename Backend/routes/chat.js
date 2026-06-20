76374b6477aa64eedd29cd41473f4d60a3cb4005
SujalPawar <pawarsujal4455@gmail.com>
2026-06-20 19:19:40 +0530

    modify chat.js file

--- Backend/routes/chat.js
+++ Backend/routes/chat.js
@@ -35,6 +35,22 @@ router.get("/thread",async(req,res)=>{
      
 })
 
+router.get("/thread/:threadId", async(req, res) => {
+    const {threadId} = req.params;
+
+    try {
+        const thread = await Thread.findOne({threadId});
+
+        if(!thread) {
+            res.status(404).json({error: "Thread not found"});
+        }
+
+        res.json(thread.messages);
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({error: "Failed to fetch chat"});
+    }
+});
 
 
 export default router;
