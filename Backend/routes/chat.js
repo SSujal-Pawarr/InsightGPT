3db26c29e110780c3084295737aa85baf72bf1ad
SujalPawar <pawarsujal4455@gmail.com>
2026-06-21 18:35:52 +0530

    modify chat.js file

--- Backend/routes/chat.js
+++ Backend/routes/chat.js
@@ -1,5 +1,6 @@
 import express from "express";
 import Thread from "../models/Thread.js";
+import getOpenAIAPIResponse from "../utils/openai.js";
 
 const router=express.Router();
 
@@ -70,6 +71,38 @@ router.delete("/thread/:threadId", async (req, res) => {
     }
 });
 
+router.post("/chat", async(req, res) => {
+    const {threadId, message} = req.body;
 
+    if(!threadId || !message) {
+        res.status(400).json({error: "missing required fields"});
+    }
+
+    try {
+        let thread = await Thread.findOne({threadId});
+
+        if(!thread) {
+            //create a new thread in Db
+            thread = new Thread({
+                threadId,
+                title: message,
+                messages: [{role: "user", content: message}]
+            });
+        } else {
+            thread.messages.push({role: "user", content: message});
+        }
+
+        const assistantReply = await getOpenAIAPIResponse(message);
+
+        thread.messages.push({role: "assistant", content: assistantReply});
+        thread.updatedAt = new Date();
+
+        await thread.save();
+        res.json({reply: assistantReply});
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({error: "something went wrong"});
+    }
+});
 
 export default router;
