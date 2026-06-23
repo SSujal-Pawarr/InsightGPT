c4d0a4b5d78a49ca21a2cf4b6762068490203061
SujalPawar <pawarsujal4455@gmail.com>
2026-06-23 08:28:47 +0530

    modify Frontend file

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -1,12 +1,25 @@
 import "./Sidebar.css";
 
 function Sidebar() {
-    return{
-       <div>
-           Sidebar
-       </div>
-    }
-    
+    return (
+        <section className="sidebar">
+            <button onClick={createNewChat}>
+                <img src="src/assets/blacklogo.png" alt="gpt logo" className="logo" />
+                <span><i className="fa-solid fa-pen-to-square"></i></span>
+            </button>
+
+
+            <ul className="history">
+               <li>history1</li>
+                <li>history2</li>
+                 <li>History3</li>
+            </ul>
+ 
+              <div className="sign">
+                <p>By ApnaCollege &hearts;</p>
+            </div>
+        </section>
+    );
 }
 
 export default Sidebar;
