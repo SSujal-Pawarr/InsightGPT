1557eaf0d3edba5c5d6ff67869bdb512ada1edad
SujalPawar <pawarsujal4455@gmail.com>
2026-06-24 18:40:25 +0530

    modify Frontend file

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -1,10 +1,46 @@
 import "./ChatWindow.css";
+import Chat from "./Chat.jsx";
 
 function ChatWindow() {
     return{
-       <div>
-          ChatWindow
-       </div>
+       return (
+        <div className="chatWindow">
+            <div className="navbar">
+                <span>SigmaGPT <i className="fa-solid fa-chevron-down"></i></span>
+                <div className="userIconDiv" onClick={handleProfileClick}>
+                    <span className="userIcon"><i className="fa-solid fa-user"></i></span>
+                </div>
+            </div>
+            {
+                isOpen && 
+                <div className="dropDown">
+                    <div className="dropDownItem"><i class="fa-solid fa-gear"></i> Settings</div>
+                    <div className="dropDownItem"><i class="fa-solid fa-cloud-arrow-up"></i> Upgrade plan</div>
+                    <div className="dropDownItem"><i class="fa-solid fa-arrow-right-from-bracket"></i> Log out</div>
+                </div>
+            }
+            <Chat></Chat>
+
+            <ScaleLoader color="#fff" loading={loading}>
+            </ScaleLoader>
+            
+            <div className="chatInput">
+                <div className="inputBox">
+                    <input placeholder="Ask anything"
+                        value={prompt}
+                        onChange={(e) => setPrompt(e.target.value)}
+                        onKeyDown={(e) => e.key === 'Enter'? getReply() : ''}
+                    >
+                           
+                    </input>
+                    <div id="submit" onClick={getReply}><i className="fa-solid fa-paper-plane"></i></div>
+                </div>
+                <p className="info">
+                    SigmaGPT can make mistakes. Check important info. See Cookie Preferences.
+                </p>
+            </div>
+        </div>
+    )
     }
     
 }
