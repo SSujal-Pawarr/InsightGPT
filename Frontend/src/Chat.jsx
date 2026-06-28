abf28aec4d55a9090635e81a715ce706832cbb87
SujalPawar <pawarsujal4455@gmail.com>
2026-06-28 09:49:24 +0530

    modify Frontend file

--- Frontend/src/Chat.jsx
+++ Frontend/src/Chat.jsx
@@ -1,8 +1,46 @@
 import "./Chat.css";
+import React, { useContext, useState, useEffect } from "react";
 
 function Chat() {
+        const {newChat, prevChats, reply} = useContext(MyContext);
+
     return (
-       <></>
+       <>
+        {newChat && <h1>Start a New Chat!</h1>}
+        <div className="chats">
+                {
+                    prevChats?.slice(0, -1).map((chat, idx) => 
+                        <div className={chat.role === "user"? "userDiv" : "gptDiv"} key={idx}>
+                            {
+                                chat.role === "user"? 
+                                <p className="userMessage">{chat.content}</p> : 
+                                <ReactMarkdown rehypePlugins={[rehypeHighlight]}>{chat.content}</ReactMarkdown>
+                            }
+                        </div>
+                    )
+                }
+
+                {
+                    prevChats.length > 0  && (
+                        <>
+                            {
+                                latestReply === null ? (
+                                    <div className="gptDiv" key={"non-typing"} >
+                                    <ReactMarkdown rehypePlugins={[rehypeHighlight]}>{prevChats[prevChats.length-1].content}</ReactMarkdown>
+                                </div>
+                                ) : (
+                                    <div className="gptDiv" key={"typing"} >
+                                     <ReactMarkdown rehypePlugins={[rehypeHighlight]}>{latestReply}</ReactMarkdown>
+                                </div>
+                                )
+
+                            }
+                        </>
+                    )
+                }
+
+            </div>
+        </>
     );
 }
 
