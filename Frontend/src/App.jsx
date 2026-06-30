48d98929af8f4779dc7152f7c29a13142e69148a
SujalPawar <pawarsujal4455@gmail.com>
2026-06-30 20:14:12 +0530

    modify Frontend file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,13 +6,13 @@ import { useState } from 'react';
 import {v1 as uuidv1} from "uuid";
 
 function App() {
-    const [prompt, setPrompt] = useState("");
+  const [prompt, setPrompt] = useState("");
   const [reply, setReply] = useState(null);
-     const [currThreadId, setCurrThreadId] = useState(uuidv1());
+  const [currThreadId, setCurrThreadId] = useState(uuidv1());
   const [prevChats, setPrevChats] = useState([]); //stores all chats of curr threads
   const [newChat, setNewChat] = useState(true);
 
-   const providerValues = {
+  const providerValues = {
       prompt, setPrompt,
     reply, setReply,
         currThreadId, setCurrThreadId,
